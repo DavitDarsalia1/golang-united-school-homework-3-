c4359afe2b888c63556cb4cbc90f6238aae33310
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-13 17:53:37 +0400

    #

--- task03-map.go
+++ task03-map.go
@@ -3,12 +3,10 @@ package homework
 import "sort"
 
 func sortMapValues(input map[int]string) (result []string) {
-
 	for k := range input {
 		result = append(result, input[k])
 	}
 
 	sort.Strings(result)
-
 	return
 }
