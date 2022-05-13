1bd93e7a6b01ec2ea41a89efd58e51c388e29c89
DavitDarsalia1 <darsalia.davit1998@gmail.com>
2022-05-13 18:42:00 +0400

    Correct Solution V2 Of Sorting Maps

--- task03-map.go
+++ task03-map.go
@@ -1,12 +1,19 @@
 package homework
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func sortMapValues(input map[int]string) (result []string) {
+	keys := make([]int, 0, 0)
 	for k := range input {
-		result = append(result, input[k])
+		keys = append(keys, k)
 	}
-
-	sort.Strings(result)
+	sort.Ints(keys)
+	for i, _ := range keys {
+		fmt.Println(input[i])
+		result = append(result, input[i+1])
+	}
 	return
 }
